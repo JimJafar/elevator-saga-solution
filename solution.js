eadd3c2b0176b4f33e03b79d7db76df3ce7a597e
Jim Sangwine <mort8104@gmail.com>
2015-01-27 14:09:31 +0100

    Queueing for people waiting & logic for picking up

--- solution.js
+++ solution.js
@@ -1,5 +1,39 @@
 {
     init: function(elevators, floors) {
+        var peopleWaitingAtFloors = {};
+        
+        floors.forEach(function(floor) {
+            peopleWaitingAtFloors[floor.floorNum()] = { up: false, down: false };
+        });
+        
+        var getDirection = function(elevator) {
+            if(elevator.destinationQueue.length > 0) {
+                return (elevator.currentFloor < elevator.destinationQueue[0]) ? 'up' : 'down';
+            } else {
+                return 'idle';
+            }
+        };
+        
+        var isPassing = function(elevator, floorNum, direction) {
+            var elevatorDirection = getDirection(elevator);
+            if (elevatorDirection !== direction) {
+                return false;
+            }
+            
+            return (elevatorDirection === 'up' && floorNum > elevator.currentFloor && floorNum < elevator.destinationQueue[0]) ||
+                   (elevatorDirection === 'down' && floorNum < elevator.currentFloor && floorNum > elevator.destinationQueue[0]);
+        };
+        
+        var setDirectionIndicators = function(elevator) {
+            if(elevator.currentFloor < elevator.destinationQueue[0]) {
+                elevator.goingUpIndicator(true);
+                elevator.goingDownIndicator(false);
+            } else {
+                elevator.goingUpIndicator(false);
+                elevator.goingDownIndicator(true);
+            }
+        };
+        
         var queueIt = function(elevator, floorNum) {
             
             if (elevator.destinationQueue.indexOf(floorNum) === -1) {
@@ -24,45 +58,80 @@
             }
         };
         
+        var pickUp = function(elevator, direction) {
+            console.log('attempting pickup');
+            console.log(peopleWaitingAtFloors);
+            var found = false;
+            if(direction === 'up') {
+                for (var i=elevator.currentFloor; i<floors.length; i++) {
+                    if(peopleWaitingAtFloors[i].up) {
+                        elevator.goToFloor(i);
+                        peopleWaitingAtFloors[i].up = false;
+                        found = true;
+                        break;
+                    }
+                }
+            } else {
+                for (var i=elevator.currentFloor; i>0; i--) {
+                    if(peopleWaitingAtFloors[i].down) {
+                        elevator.goToFloor(i);
+                        peopleWaitingAtFloors[i].down = false;
+                        found = true;
+                        break;
+                    }
+                }
+            }
+            if(found) { console.log('found someone!'); } else { console.log('found noone!'); }
+            return found;
+        };
+        
         elevators.forEach(function(elevator) {
             elevator.on("idle", function() {
-                elevator.goToFloor(0);
+                if(elevator.currentFloor < floors.length/2) {
+                    if (!pickUp(elevator, 'up')) {
+                        if(!pickUp(elevator, 'down')) {
+                            elevator.goToFloor(0);
+                        }
+                    }
+                } else {
+                    if (!pickUp(elevator, 'down')) {
+                        if (!pickUp(elevator, 'up')) {
+                            elevator.goToFloor(0);
+                        }
+                    }
+                }
+                
             });
             elevator.on('floor_button_pressed', function(floorNum) {
                 queueIt(elevator, floorNum);
             });
             elevator.on("passing_floor", function(floorNum, direction) {
-                
+                setDirectionIndicators(elevator);
+                if (peopleWaitingAtFloors[floorNum][direction]) {
+                    elevator.goToFloor(floorNum, true);
+                }
+            });
+            elevator.on("stopped_at_floor", function(floorNum) {
+                peopleWaitingAtFloors[floorNum][getDirection(elevator)] = false;
+                if(floorNum === 0) { elevator.goingUpIndicator(true); elevator.goingDownIndicator(false); }
             });
         });
         
         floors.forEach(function(floor) {
             var handleFloorButtonPress = function(direction) {
-                if (elevators.filter(function(elevator) { return elevator.destinationQueue.indexOf(floor.floorNum) !== -1; }).length > 0) {
-                    return;
-                }
-                
                 var selectedElevator = null;
                 
-                var potentialElevators = elevators.filter(function(elevator) {
-                    return elevator.loadFactor() < 0.7 &&
-                           ((direction === 'up' && elevator.currentFloor < floor.floorNum && elevator.goingUpIndicator()) ||
-                           (direction === 'down' && elevator.currentFloor > floor.floorNum && elevator.goingDownIndicator()));
+                elevators.forEach(function(elevator) {
+                    if(isPassing(elevator, floor.floorNum(), direction)) {
+                        selectedElevator = elevator;
+                    }
                 });
                 
-                if (potentialElevators.length === 0) {
-                    selectedElevator = elevators.sort(function(a,b) {
-                        return (Math.abs(a.currentFloor-floor.floorNum) < Math.abs(b.currentFloor-floor.floorNum())) ? -1 : 1;
-                    })[0];
-                } else if (potentialElevators.length === 1) {
-                    selectedElevator = potentialElevators[0];
+                if(selectedElevator) {
+                    elevator.goToFloor(floor.floorNum(), true);
                 } else {
-                    selectedElevator = potentialElevators.sort(function(a,b) {
-                        return (Math.abs(a.currentFloor-floor.floorNum) < Math.abs(b.currentFloor-floor.floorNum())) ? -1 : 1;
-                    })[0];
+                    peopleWaitingAtFloors[floor.floorNum()][direction] = true;
                 }
-                
-                queueIt(selectedElevator, floor.floorNum());
             };
             floor.on("up_button_pressed", function() {
                 handleFloorButtonPress('up');
